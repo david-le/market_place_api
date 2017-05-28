f76238d33c4e5a8d30ea56e1c19b334503089d3f
David Le <davidle.iosdev@gmail.com>
2017-05-28 00:32:11 -0400

    Finished Tutorial 2

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,18 @@
 require File.expand_path('../boot', __FILE__)
 
+# don't generate RSpec tests for views and helpers
+  config.generators do |g|
+    g.test_framework :rspec, fixture: true
+    g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    g.view_specs false
+    g.helper_specs false
+    g.stylesheets = false
+    g.javascripts = false
+    g.helper = false
+  end
+
+  config.autoload_paths += %W(\#{config.root}/lib)
+
 # Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ MarketPlaceApi::Application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/' do
-   scope module: :v1 do
-     # List resources here
+   scope module: :v1,
+            constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
    end
   end
 end
