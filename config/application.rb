89c616d34be7834fab38726b994ff9f18cdceddf
David Le <davidle.iosdev@gmail.com>
2017-05-28 00:41:49 -0400

    Fixing errors

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,5 @@
 require File.expand_path('../boot', __FILE__)
 
-# don't generate RSpec tests for views and helpers
-  config.generators do |g|
-    g.test_framework :rspec, fixture: true
-    g.fixture_replacement :factory_girl, dir: 'spec/factories'
-    g.view_specs false
-    g.helper_specs false
-    g.stylesheets = false
-    g.javascripts = false
-    g.helper = false
-  end
-
-  config.autoload_paths += %W(\#{config.root}/lib)
-
 # Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
@@ -37,5 +24,18 @@ module MarketPlaceApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # don't generate RSpec tests for views and helpers
+      config.generators do |g|
+        g.test_framework :rspec, fixture: true
+        g.fixture_replacement :factory_girl, dir: 'spec/factories'
+        g.view_specs false
+        g.helper_specs false
+        g.stylesheets = false
+        g.javascripts = false
+        g.helper = false
+      end
+
+      config.autoload_paths += %W(\#{config.root}/lib)
   end
 end
